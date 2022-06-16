b966e8b1ccea8bfbdb794c3fb0a00e511c183cff
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-06-16 17:10:49 +0630

    Bug fixed

--- SimpleNavbar/scripts.js
+++ SimpleNavbar/scripts.js
@@ -1,8 +1,12 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-     }
-  }
+//Navigation Bar
+function iconClick(){
+  location.href="index.html";
+}
+function navbar() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+   }
+}
