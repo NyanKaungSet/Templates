6084ecbd8b1f6f2ec29239624e3421b036929cbe
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-07-08 11:48:30 +0630

    Add files via upload

---
+++ NavbarWithDropdown/script.js
@@ -0,0 +1,27 @@
+//Navigation Bar
+function iconClick(){
+  location.href="index.html";
+}
+function navbar() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+   }
+}
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
