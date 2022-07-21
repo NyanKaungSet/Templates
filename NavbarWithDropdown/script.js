2f6dbddf07635a2217c2b4d6f6e0b8fe83b4ea1e
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-07-21 12:02:10 +0630

    Issue #1 fixed
    Work well
    
    But still not ***RESPONSIVE***

--- NavbarWithDropdown/script.js
+++ NavbarWithDropdown/script.js
@@ -11,7 +11,8 @@ function navbar() {
    }
 }
 
-
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
